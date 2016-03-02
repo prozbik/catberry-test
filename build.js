de6ddfad09e0cd2dea8b25ac372668d84b073f49
prozbik <prozbik@gmail.com>
2016-03-02 15:02:14 +0200

    added catberry-assets

--- build.js
+++ build.js
@@ -3,11 +3,12 @@
 var isRelease = process.argv.length === 3 ?
 		process.argv[2] === 'release' : undefined,
 	templateEngine = require('catberry-jade'),
+	assets = require('catberry-assets'),
 	catberry = require('catberry'),
 	cat = catberry.create({
 		isRelease: isRelease
 	});
-
+	
+assets.register(cat.locator);
 templateEngine.register(cat.locator);
 cat.build();
-
